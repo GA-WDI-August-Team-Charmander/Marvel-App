22641a506ef6286adcd88f9b71e58bc5124a0291
AdrianMLin <adrian.mx.lin@gmail.com>
2014-10-10 11:09:19 -0400

    updated <models.rb> associations

--- lib/models.rb
+++ lib/models.rb
@@ -8,28 +8,39 @@ class User < ActiveRecord::Base
 end
 
 class Character < ActiveRecord::Base
-  # self.has_many(:fav_characters) or is it belongs to?
+  has_many(:comic_chars)
+  has_many(:fav_characters)
 end
 
 class Comic < ActiveRecord::Base
-  # 
+  has_many(:characters_comics)
+  has_many :users_comics
 end
 
 class Badge < ActiveRecord::Base
+  has_many :users_badges 
 end
 
 # **<{{ Join tables }}>** -----------------------------
 
 class Fav_character < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :character
 end
 
 class Comic_appearance < ActiveRecord::Base
+  belongs_to :comic
+  belongs_to :character
 end
 
 class Users_comic < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :comic
 end
 
 class Users_badge < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :comic
 end
 
 

127499816e9c214f21b32de80b9a4be148fb02f4
AdrianMLin <adrian.mx.lin@gmail.com>
2014-10-10 11:46:55 -0400

    models and stuff

--- db/schema.sql
+++ db/schema.sql
@@ -13,12 +13,6 @@ image_url text,
 description text
 );
 
-CREATE TABLE fav_characters (
-id serial primary key,
-user_id integer,
-character_id integer
-);
-
 CREATE TABLE comics (
 id serial primary key,
 title varchar(255),
@@ -28,6 +22,11 @@ isbn varchar(55),
 page_count integer,
 series text,
 thumbnail text
+);
+
+CREATE TABLE badges (
+id serial primary key,
+name varchar(255)
 );
 
 CREATE TABLE characters_comics (
@@ -40,14 +39,17 @@ CREATE TABLE users_comics (
 id serial primary key,
 comic_id integer,
 user_id integer,
-read varchar(255), # read(false) means to read
-bought boolean # bought(false) means to buy 
+read varchar(255), 
+bought boolean 
 );
 
-CREATE TABLE badges (
+
+CREATE TABLE fav_characters (
 id serial primary key,
-name varchar(255),
+user_id integer,
+character_id integer
 );
+
 
 CREATE TABLE users_badges (
 id serial primary key,

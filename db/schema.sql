29928cbcf6f247a7f5ce98a5bb989d3d42a02765
JoeBiggia <joe.biggica@gmail.com>
2014-10-10 11:49:21 -0400

    seed in progress

--- db/schema.sql
+++ db/schema.sql
@@ -13,12 +13,6 @@ image_url text,
 description text
 );
 
-CREATE TABLE fav_characters (
-id serial primary key,
-user_id integer,
-character_id integer
-);
-
 CREATE TABLE comics (
 id serial primary key,
 title varchar(255),
@@ -28,6 +22,17 @@ isbn varchar(55),
 page_count integer,
 series text,
 thumbnail text
+);
+
+CREATE TABLE badges (
+id serial primary key,
+name varchar(255)
+);
+
+CREATE TABLE fav_characters (
+id serial primary key,
+user_id integer,
+character_id integer
 );
 
 CREATE TABLE characters_comics (
@@ -40,13 +45,8 @@ CREATE TABLE users_comics (
 id serial primary key,
 comic_id integer,
 user_id integer,
-read varchar(255), # read(false) means to read
-bought boolean # bought(false) means to buy 
-);
-
-CREATE TABLE badges (
-id serial primary key,
-name varchar(255),
+read varchar(255),
+bought boolean
 );
 
 CREATE TABLE users_badges (

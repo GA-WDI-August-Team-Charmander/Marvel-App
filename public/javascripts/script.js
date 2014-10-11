ba65d2b7b90d744eab63d5d33dbf9512e4e2ba31
AdrianMLin <adrian.mx.lin@gmail.com>
2014-10-11 11:42:35 -0400

    commented out script.js and reorganised. also added div#users. || trying to build some visuals and then test out

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,5 @@
 
-//~<*{{ MODEL }}*>~ --------------------------------
+//~<*{{ User }}*>~ --------------------------------
 
 var User_Model = Backbone.Model.extend({
 	initialize: function(){
@@ -9,127 +9,178 @@ var User_Model = Backbone.Model.extend({
 	urlRoot: '/users'
 });
 
-var Character_Model = Backbone.Model.extend({
-	initialize: function(){
-		console.log("New character initialised!");
-	},
-
-	urlRoot: '/characters'
-});
-
-var Comic_Model = Backbone.Model.extend({
-	initialize: function(){
-		console.log("New comic initialised!");
-	},
-
-	urlRoot: '/comics'
-});
-
-var Badge_Model = Backbone.Model.extend({
-	initialize: function(){
-		console.log("New Badge initialised!");
-	},
-
-	urlRoot: '/badges'
-});
-
-var FavCharacter_Model = Backbone.Model.extend({
-	url: '/users/' + id + "/fav_characters",
-
-	initialize: function(){
-		console.log("New Fav_character initialised!");
-	},
-
-	urlRoot: '/fav_characters'
-});
-
-var CharactersComic_Model = Backbone.Model.extend({
-	initialize: function(){
-		console.log("New Characters_comic initialised!");
-	},
-
-	urlRoot: '/characters_comics'
-});
-
-var UsersComic_Model = Backbone.Model.extend({
-	initialize: function(){
-		console.log("New Users_comic initialised!");
-	},
-
-	urlRoot: '/users_comics'
-});
-
-var UsersBadge_Model = Backbone.Model.extend({
-	initialize: function(){
-		console.log("New users_badge initialised!");
-	},
-
-	urlRoot: '/users_badges'
-
-});
-
-
-//~<*{{ COLLECTION }}*>~ --------------------------------
-
 var User_Collection  = Backbone.Collection.extend({	
 	model: User_Model,
 	url: '/users'
 });
-var user_collection = new UserCollection
 
-var Comic_Collection = Backbone.Collection.extend({
-	model: Comic_Model,
-	url: '/comics'
+var user_list_view = new List_View( {collection: User_Collection} );
+
+
+
+
+
+var Item_View = Backbone.View.extend({
+	tagName: "li",
+
+	initialize: function(){
+		this.listenTo(this.model, "remove", this.remove);
+	},
+	render: function(){
+		this.$el.html("<p>" + this.model.get('name') + "</p>");
+	}
+
 });
 
+var List_View = Backbone.View.extend({
+	tagName: "ul",
 
-var Character_Collection = Backbone.Collection.extend({
-	model: Character_Model,
-	url: '/characters'
-});
-//chars
+	initialize: function(){
+		this.listenTo(this.collection, 'add', this.addOne);
+		this.collection.fetch();
+	},
 
-var Badge_Collection = Backbone.Collection.extend({
-	model: Badge_Model,
-	url: '/badges'
-});
-//badge
-
-var FavCharacter_Collection = Backbone.Collection.extend({
-	model: FavCharacter_Model,
-	//url:  //?????????????
-	//url: function(){
-	//	return 
-	//}
-});
-var favCharacter_collection = new FavCharacter_Collection();
-//favChars
-
-var UsersBadge_Collection = Backbone.Collection.extend({
-	model: UsersBadge_Model,
-	//url: '/users/' + id + "/badges"
+	addOne: function(item_view){
+		var item_view = new Item_view({model: item_view});
+		item_view.render();
+		this.$el.append(item_view);
+	}
 });
 
-// var CharactersComic_Collection  = Backbone.Collection.extend({
-// 	model: CharactersComic_Model,
-// 	url: '/'
+function generatePage(){
+	$('#users').append(user_list_view)
+}
+
+generatePage();
+
+
+
+
+
+
+
+
+
+
+
+// var Character_Model = Backbone.Model.extend({
+// 	initialize: function(){
+// 		console.log("New character initialised!");
+// 	},
+
+// 	urlRoot: '/characters'
+// });
+
+// var Comic_Model = Backbone.Model.extend({
+// 	initialize: function(){
+// 		console.log("New comic initialised!");
+// 	},
+
+// 	urlRoot: '/comics'
+// });
+
+// var Badge_Model = Backbone.Model.extend({
+// 	initialize: function(){
+// 		console.log("New Badge initialised!");
+// 	},
+
+// 	urlRoot: '/badges'
+// });
+
+// var FavCharacter_Model = Backbone.Model.extend({
+// 	url: '/users/' + id + "/fav_characters",
+
+// 	initialize: function(){
+// 		console.log("New Fav_character initialised!");
+// 	},
+
+// 	urlRoot: '/fav_characters'
+// });
+
+// var CharactersComic_Model = Backbone.Model.extend({
+// 	initialize: function(){
+// 		console.log("New Characters_comic initialised!");
+// 	},
+
+// 	urlRoot: '/characters_comics'
+// });
+
+// var UsersComic_Model = Backbone.Model.extend({
+// 	initialize: function(){
+// 		console.log("New Users_comic initialised!");
+// 	},
+
+// 	urlRoot: '/users_comics'
+// });
+
+// var UsersBadge_Model = Backbone.Model.extend({
+// 	initialize: function(){
+// 		console.log("New users_badge initialised!");
+// 	},
+
+// 	urlRoot: '/users_badges'
+
 // });
 
 
-var Task_Collection = Backbone.Collection.extend({
-	model: UsersComic_Model,
-	url: '/users/' + user_id + "/com"
-});
-//toReadComics, ReadComics, toBuyComics, boughtComics
+// //~<*{{ COLLECTION }}*>~ --------------------------------
+
+
+// var user_collection = new UserCollection
+
+// var Comic_Collection = Backbone.Collection.extend({
+// 	model: Comic_Model,
+// 	url: '/comics'
+// });
+
+
+// var Character_Collection = Backbone.Collection.extend({
+// 	model: Character_Model,
+// 	url: '/characters'
+// });
+// //chars
+
+// var Badge_Collection = Backbone.Collection.extend({
+// 	model: Badge_Model,
+// 	url: '/badges'
+// });
+// //badge
+
+// var FavCharacter_Collection = Backbone.Collection.extend({
+// 	model: FavCharacter_Model,
+// 	//url:  //?????????????
+// 	//url: function(){
+// 	//	return 
+// 	//}
+// });
+// var favCharacter_collection = new FavCharacter_Collection();
+// //favChars
+
+// var UsersBadge_Collection = Backbone.Collection.extend({
+// 	model: UsersBadge_Model,
+// 	//url: '/users/' + id + "/badges"
+// });
+
+// // var CharactersComic_Collection  = Backbone.Collection.extend({
+// // 	model: CharactersComic_Model,
+// // 	url: '/'
+// // });
+
+
+// var Task_Collection = Backbone.Collection.extend({
+// 	model: UsersComic_Model,
+// 	url: '/users/' + user_id + "/com"
+// });
+// //toReadComics, ReadComics, toBuyComics, boughtComics
 
 
 
-var toBuyComics;
-var boughtComics;
-var toReadComics;
-var readComics;
-var badges;
-var favChars;
+// var toBuyComics;
+// var boughtComics;
+// var toReadComics;
+// var readComics;
+// var badges;
+// var favChars;
 
 
 
@@ -137,39 +188,9 @@ var favChars;
 //~<*{{ VIEW }}*>~ --------------------------------
 
 
-var Task_View = Backbone.View.extend({
-	tagName: "li",
-
-	initialize: function(){
-		this.listenTo(this.model, "remove", this.remove)
-	},
-	render: function(){
-
-	},
-
-	attributes: {},
-
-	events: {}
-
-
-});
 
 
 
-var List_View = Backbone.View.extend({
-	tagName: "ul",
-	
-	initialize: function(){
-		this.listenTo(this.collection, 'add', this.addOne);
-		this.collection.fetch();
-	},
-
-	addOne: function(task_view){
-		var task_view = new Task_View({model: task_view});
-		task_view.render();
-		this.$el.append(task_view);
-	}
-});
 
 
 
